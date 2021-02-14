9b71965a320f53dad1b614f62084b07b9daa3884
YangX-gh <378580085@qq.com>
2021-02-14 20:40:29 +0800

    deepsort

---
+++ deep_sort/detectron2_deepsort.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import time
+from distutils.util import strtobool
+
+import cv2
+
+from deep_sort.Deep_sort import DeepSort
+from deep_sort.detectron2_detection import Detectron2
+from deep_sort.util import draw_bboxes
+
+
+class Detector(object):
+    def __init__(self, args):
+        self.args = args
+        use_cuda = bool(strtobool(self.args.use_cuda))
+        if args.display:
+            cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("test", args.display_width, args.display_height)
+
+        self.vdo = cv2.VideoCapture()
+        self.detectron2 = Detectron2()
+
+        self.deepsort = DeepSort(args.deepsort_checkpoint, use_cuda=use_cuda)
+
+    def __enter__(self):
+        assert os.path.isfile(self.args.VIDEO_PATH), "Error: path error"
+        self.vdo.open(self.args.VIDEO_PATH)
+        self.im_width = int(self.vdo.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.im_height = int(self.vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        if self.args.save_path:
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            self.output = cv2.VideoWriter(self.args.save_path, fourcc, 20, (self.im_width, self.im_height))
+
+        assert self.vdo.isOpened()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type:
+            print(exc_type, exc_value, exc_traceback)
+
+    def detect(self):
+        while self.vdo.grab():
+            start = time.time()
+            _, im = self.vdo.retrieve()
+            # im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+            bbox_xcycwh, cls_conf, cls_ids = self.detectron2.detect(im)
+
+            if bbox_xcycwh is not None:
+                # select class person
+                mask = cls_ids == 0
+
+                bbox_xcycwh = bbox_xcycwh[mask]
+                bbox_xcycwh[:, 3:] *= 1.2
+
+                cls_conf = cls_conf[mask]
+                outputs = self.deepsort.update(bbox_xcycwh, cls_conf, im)
+                if len(outputs) > 0:
+                    bbox_xyxy = outputs[:, :4]
+                    identities = outputs[:, -1]
+                    im = draw_bboxes(im, bbox_xyxy, identities)
+
+            end = time.time()
+            print("time: {}s, fps: {}".format(end - start, 1 / (end - start)))
+
+            if self.args.display:
+                cv2.imshow("test", im)
+                cv2.waitKey(1)
+
+            if self.args.save_path:
+                self.output.write(im)
+            # exit(0)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("VIDEO_PATH", type=str)
+    parser.add_argument("--deepsort_checkpoint", type=str, default="deep_sort/deep/checkpoint/ckpt.t7")
+    parser.add_argument("--max_dist", type=float, default=0.3)
+    parser.add_argument("--ignore_display", dest="display", action="store_false")
+    parser.add_argument("--display_width", type=int, default=800)
+    parser.add_argument("--display_height", type=int, default=600)
+    parser.add_argument("--save_path", type=str, default="demo.avi")
+    parser.add_argument("--use_cuda", type=str, default="True")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    with Detector(args) as det:
+        det.detect()
---
+++ deep_sort/detectron2_detection.py
@@ -0,0 +1,43 @@
+import Detection_model
+from Detection_model.detectron2.detectron2.utils.logger import setup_logger
+setup_logger()
+
+import numpy as np
+from Detection_model.detectron2.detectron2.engine import DefaultPredictor
+from Detection_model.detectron2.detectron2.config import get_cfg
+from Detection_model.detectron2.detectron2 import model_zoo
+
+
+class Detectron2:
+
+    def __init__(self):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def bbox(self, img):
+        rows = np.any(img, axis=1)
+        cols = np.any(img, axis=0)
+        rmin, rmax = np.where(rows)[0][[0, -1]]
+        cmin, cmax = np.where(cols)[0][[0, -1]]
+        return cmin, rmin, cmax, rmax
+
+    def detect(self, im):
+        outputs = self.predictor(im)
+        boxes = outputs["instances"].pred_boxes.tensor.cpu().numpy()
+        classes = outputs["instances"].pred_classes.cpu().numpy()
+        scores = outputs["instances"].scores.cpu().numpy()
+
+        bbox_xcycwh, cls_conf, cls_ids = [], [], []
+
+        for (box, _class, score) in zip(boxes, classes, scores):
+
+            if _class == 0:
+                x0, y0, x1, y1 = box
+                bbox_xcycwh.append([(x1 + x0) / 2, (y1 + y0) / 2, (x1 - x0), (y1 - y0)])
+                cls_conf.append(score)
+                cls_ids.append(_class)
+
+        return np.array(bbox_xcycwh, dtype=np.float64), np.array(cls_conf), np.array(cls_ids)
