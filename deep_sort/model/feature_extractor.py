bd7040da545c41a5f5524ac26b64757b5cdbf8bf
YangX-gh <78676492+YangX-gh@users.noreply.github.com>
2021-04-22 22:41:23 +0800

    add and modify

--- deep_sort/model/feature_extractor.py
+++ deep_sort/model/feature_extractor.py
@@ -7,7 +7,8 @@ from .model import Net
 
 class Extractor(object):
     def __init__(self, model_path, use_cuda=True):
-        self.net = Net(reid=True)
+        self.net = Net(num_classes=6, reid=True)
+        # self.net = Net(reid=True)
         self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
         state_dict = torch.load(model_path, map_location=lambda storage, loc: storage)['net_dict']
         self.net.load_state_dict(state_dict)
